f1cded7e4c43fe1f2a45b5baffdea0c8a8be7198
Adilson de Almeida Neto <almeidneto@gmail.com>
2016-12-05 05:00:54 -0300

    Hotfix #2

--- routes/frequency.js
+++ routes/frequency.js
@@ -1,5 +1,11 @@
+var mongoose = require('mongoose');
+var models = require('../models');
+var helpers = require('../helpers')
+
+var Frequency = mongoose.model('Frequency');
+
 module.exports = {
   get: function(req, res) {
-    Profile.find({project: req.query.projectId}, helpers.client.findAll(req, res, "Frequency"));
+    Frequency.find({project: req.query.projectId}, helpers.client.findAll(req, res, "Frequency"));
   }
 }
