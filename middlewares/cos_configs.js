a88c2da92193a0bf7b00c2f1cd97121cb405d52f
Adilson de Almeida Neto <almeidneto@gmail.com>
2016-12-20 06:49:53 -0300

    a

--- middlewares/cos_configs.js
+++ middlewares/cos_configs.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Content-Type, Accept, X-HTTP-Method-Override');
   if ('OPTIONS' === req.method) {
     res.sendStatus(200);
     res.end();
