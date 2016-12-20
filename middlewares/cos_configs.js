630e03ad8946669544c3d182fa68eec4bb7d84bd
Adilson de Almeida Neto <almeidneto@gmail.com>
2016-12-20 06:41:23 -0300

    a

--- middlewares/cos_configs.js
+++ middlewares/cos_configs.js
@@ -4,7 +4,6 @@ module.exports = function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   if ('OPTIONS' === req.method) {
     res.sendStatus(200);
-    res.send("bla");
     res.end();
   } else {
     next();
