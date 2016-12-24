8f786156a2128fcb3e0d26d70644994707319ceb
Adilson de Almeida Neto <almeidneto@gmail.com>
2016-12-24 04:43:30 -0300

    a

--- routes/project.js
+++ routes/project.js
@@ -81,67 +81,71 @@ module.exports = {
   },
   // id for the project, userId for user id
   postMember: function(req, res) {
-    var profileId = jwt.decode(req.token, config.secret)._id;
-    Profile.findOne({_id: profileId}, function(error, profile) {
-      if (error) {
-        console.log(error);
-      } else if(profile) {
-        Project.findOne({_id: req.body.project}, function(error, obj) {
-          if(error) {
-            console.log(error);
-          } else if(obj) {
-            if (has_level(obj.members, req.body.level, profileId) || profile.is_admin) {
-              if (req.body.operation == "remove") {
-                Project.update({_id: req.body.project}, {$pop: {members: {_id: mongoose.Types.ObjectId(req.body.profile), level: 0}}}, function(err, project) {
-                  if (err) {
-                    console.log("Something went wrong: " + err);
-                    res.status(500).send("Something went wrong: " + err);
-                    res.end();
-                  } else {
-                    Profile.update({_id: req.body.profile}, {$pop: {projects: req.body.project}}, function(error, obj) {
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        console.log(obj);
-                      }
-                    });
-                    res.json(project);
-                  }
-                });
-              }
-              else if(req.body.operation == "add"){
-                Project.update({_id: req.body.project}, {$addToSet: {members: {_id: mongoose.Types.ObjectId(req.body.profile), level: req.body.level}}}, function(err, project) {
-                  if (err) {
-                    console.log("Something went wrong: " + err);
-                    res.status(500).send("Something went wrong: " + err);
-                    res.end();
-                  } else {
-                    Profile.update({_id: req.body.profile}, {$addToSet: {projects: mongoose.Types.ObjectId(req.body.project)}}, function(error, result) {
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        console.log(result);
-                      }
-                    });
-                    res.json(project);
-                  }
-                });
+    if(req.body.id && req.body.profile && req.body.operation) {
+      var profileId = jwt.decode(req.token, config.secret)._id;
+      Profile.findOne({_id: profileId}, function(error, profile) {
+        if (error) {
+          console.log(error);
+        } else if(profile) {
+          Project.findOne({_id: req.body.project}, function(error, obj) {
+            if(error) {
+              console.log(error);
+            } else if(obj) {
+              if (has_level(obj.members, req.body.level, profileId) || profile.is_admin) {
+                if (req.body.operation == "remove") {
+                  Project.update({_id: req.body.project}, {$pop: {members: {_id: mongoose.Types.ObjectId(req.body.profile), level: 0}}}, function(err, project) {
+                    if (err) {
+                      console.log("Something went wrong: " + err);
+                      res.status(500).send("Something went wrong: " + err);
+                      res.end();
+                    } else {
+                      Profile.update({_id: req.body.profile}, {$pop: {projects: req.body.project}}, function(error, obj) {
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log(obj);
+                        }
+                      });
+                      res.json(project);
+                    }
+                  });
+                }
+                else if(req.body.operation == "add"){
+                  Project.update({_id: req.body.project}, {$addToSet: {members: {_id: mongoose.Types.ObjectId(req.body.profile), level: req.body.level}}}, function(err, project) {
+                    if (err) {
+                      console.log("Something went wrong: " + err);
+                      res.status(500).send("Something went wrong: " + err);
+                      res.end();
+                    } else {
+                      Profile.update({_id: req.body.profile}, {$addToSet: {projects: mongoose.Types.ObjectId(req.body.project)}}, function(error, result) {
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log(result);
+                        }
+                      });
+                      res.json(project);
+                    }
+                  });
+                }
+                else {
+                  res.end();
+                }
               }
               else {
+                res.send("No access level");
                 res.end();
               }
             }
-            else {
-              res.send("No access level");
-              res.end();
-            }
-          }
-        });
-      } else {
-        console.log("No profile found");
-        res.end();
-      }
-    });
+          });
+        } else {
+          console.log("No profile found");
+          res.end();
+        }
+      });
+    } else {
+      res.end();
+    }
   },
   count: function(req, res) {
     Project.count({}, helpers.client.count(req, res, "Project"))
